35c55fec63a223df65ca1137ffae81a802cda701
Budi Darmawan <vbudi@us.ibm.com>
2020-10-06 14:20:41 -0500

    v007

--- check/cp4multicloud-2.0.0.sh
+++ check/cp4multicloud-2.0.0.sh
@@ -6,7 +6,7 @@ version=$(echo ${name} | cut -d- -f2)
 
 mcmcsvphase=$(oc get csv ibm-management-hybridapp.v2.0.0 -n kube-system --no-headers -o custom-columns=mcm:status.phase 2>/dev/null)
 
-if [ $mcmcsvphase = "Succeeded" ]; then
+if [ "$mcmcsvphase" = "Succeeded" ]; then
   inst="true"
 else
   inst="false"

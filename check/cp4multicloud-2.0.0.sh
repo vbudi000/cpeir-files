914e633d46aa291f39b752715ef93af13a84e31e
Budi Darmawan <vbudi@us.ibm.com>
2020-10-06 14:07:22 -0500

    v005

--- check/cp4multicloud-2.0.0.sh
+++ check/cp4multicloud-2.0.0.sh
@@ -3,13 +3,17 @@
 filename=$(basename $0)
 name="${filename%.*}"
 version=$(echo ${name} | cut -d- -f2)
-OUT=`oc get po --no-headers=true -A | grep -v 'Running\|Completed\|gateway-kong' | grep 'kube-system\|ibm-common-services\|management-infrastructure-management\|management-monitoring\|management-operations\|management-security-services'`
-WC=$(printf "%s\n" "$OUT" | wc | awk '{print $1}')
+
+mcmcsvphase=$(oc get csv ibm-management-hybridapp.v2.0.0 -n kube-system --no-headers -o custom-columns=mcm:status.phase )
+
+if [ $mcmcsvphase -eq "Succeeded" ]; then
+  inst="true"
+else
+  inst="false"
+fi
 
 if  [[ $wc -le 0 ]]; then
   inst="true"
-  depname=$(oc get deployment -l app.kubernetes.io/instance=multicluster-hub -A --no-headers -o custom-columns=name:metadata.name | head -n 1)
-  version=$(oc label deployment ${depname} -n kube-system --list | grep helm.sh | cut -d- -f2)
 else
   inst="false"
 fi

93acc1022c82aa857d2bd65f7cac7513d940370e
Budi Darmawan <vbudi@us.ibm.com>
2020-10-06 14:16:54 -0500

    v006

--- check/cp4multicloud-2.0.0.sh
+++ check/cp4multicloud-2.0.0.sh
@@ -4,19 +4,14 @@ filename=$(basename $0)
 name="${filename%.*}"
 version=$(echo ${name} | cut -d- -f2)
 
-mcmcsvphase=$(oc get csv ibm-management-hybridapp.v2.0.0 -n kube-system --no-headers -o custom-columns=mcm:status.phase )
+mcmcsvphase=$(oc get csv ibm-management-hybridapp.v2.0.0 -n kube-system --no-headers -o custom-columns=mcm:status.phase 2>/dev/null)
 
-if [ $mcmcsvphase -eq "Succeeded" ]; then
+if [ $mcmcsvphase = "Succeeded" ]; then
   inst="true"
 else
   inst="false"
 fi
 
-if  [[ $wc -le 0 ]]; then
-  inst="true"
-else
-  inst="false"
-fi
 
 echo "{\"installed\": ${inst}, \"name\": \"${name}\", \"version\": \"${version}\"}"
 exit 0

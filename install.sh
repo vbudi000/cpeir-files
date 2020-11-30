cec7848032cbb407570069357d1f19190e2d2c24
Budi Darmawan <vbudi@us.ibm.com>
2020-11-30 14:22:24 -0600

    ready

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ oc create serviceaccount cpeir -n cpeir
 oc adm policy add-cluster-role-to-user -z cpeir -n cpeir
 
 oc create -f cloud.ibm.com_cpeirs_crd.yaml
-
+oc create secret entitlement -n cpeir --from-literal entitlementKey=$1

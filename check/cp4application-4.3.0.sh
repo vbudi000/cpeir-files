56c05700e1010739a71ebcaf025151889a93ff3d
Budi Darmawan <vbudi@us.ibm.com>
2021-01-28 13:28:40 -0600

    V2.001

---
+++ check/cp4application-4.3.0.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+filename=$(basename $0)
+name="${filename%.*}"
+version=$(echo ${name} | cut -d- -f2)
+
+csvnum=$(oc get csv -n ta --no-headers 2>/dev/null | wc -l)
+csvsuc=$(oc get csv -n ta --no-headers 2>/dev/null | grep -v "Succeeded" | wc -l)
+
+if [ "$csvnum" -gt "0" ]; then
+  if [ "$csvsuc" -eq "0" ]; then
+    inst="true"
+  else
+    inst="false"
+  fi
+else
+  inst="false"
+fi
+
+echo "{\"installed\": ${inst}, \"name\": \"${name}\", \"version\": \"${version}\"}"
+exit 0
